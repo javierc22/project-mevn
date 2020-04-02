1aebfec051a4dbacbe9d2b5a49e0710a89652f0b
javierc22 <javier.ec.22@gmail.com>
2020-04-01 22:47:52 -0300

    Hola mundo con Node.js y Express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  res.send('Hola Mundo!');
+})
+
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen(app.get('puerto'), function() {
+  console.log('Escuchando el puerto:', app.get('puerto'));
+})
