80fbb91c9edce82754c5d93196af9409f4a81c42
javierc22 <javier.ec.22@gmail.com>
2020-04-04 18:18:12 -0300

    Instalar JSON y urlencoded (middleware)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 
 app.use(morgan('tiny'));
 app.use(cors());
+app.use(express.json());
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function(req, res) {
   res.send('Hola Mundo!');
