702dd21b4390ce9ad8b8074c707ace37be1f4f00
javierc22 <javier.ec.22@gmail.com>
2020-04-04 18:33:44 -0300

    History Vue.js

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const cors = require('cors');
 // Para acceder al directorio actual
 const path = require('path');
 
-
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })) //application/x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, 'public')));
 
+// Rutas
 app.get('/', function(req, res) {
   res.send('Hola Mundo!');
 })
 
-app.set('puerto', process.env.PORT || 3000);
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), function() {
   console.log('Escuchando el puerto:', app.get('puerto'));
 })
