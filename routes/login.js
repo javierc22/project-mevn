3ad55f56fdb3f303e98dd57c64d45797cde0995e
javierc22 <javier.ec.22@gmail.com>
2020-04-11 17:32:05 -0400

    POST Login

--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,36 @@ import User from '../models/user';
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-router.get('/', async(req, res) => {
-  res.json({mensaje: 'Funciona!'})
+router.post('/', async(req, res) => {
+  const body = req.body;
+
+  try {
+    // Evaluando email
+    const usuarioDB = await User.findOne({email: body.email});
+    if (!usuarioDB) {
+      return res.status(400).json({
+        mensaje: 'Email no encontrado',
+      });
+    }
+
+    // Evaluando contraseña
+    if (!bcrypt.compareSync(body.pass, usuarioDB.pass)) {
+      return res.status(400).json({
+        mensaje: 'Contraseña incorrecta',
+      });
+    }
+
+    res.json({
+      usuarioDB,
+      token: 'tokendeprueba'
+    });
+
+  } catch (error) {
+    return res.status(400).json({
+      mensaje: 'Ocurrió un error',
+      error
+    })
+  }
 })
 
 module.exports = router;
