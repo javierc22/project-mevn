af67d3293867404c331b8cdd6e90c47e505b4984
javierc22 <javier.ec.22@gmail.com>
2020-04-11 17:09:15 -0400

    PUT User contraseña

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,9 @@ import User from '../models/user';
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+// Filtrar campos de PUT
+const _ = require('underscore');
+
 // POST Usuario
 router.post('/nuevo-usuario', async(req, res) => {
   const body = {
@@ -31,7 +34,11 @@ router.post('/nuevo-usuario', async(req, res) => {
 // PUT Usuario
 router.put('/usuario/:id', async(req, res) =>{
   const _id = req.params.id;
-  const body = req.body;
+  const body = _.pick(req.body, ['nombre', 'email', 'pass', 'activo']);
+
+  if (body.pass) {
+    body.pass = bcrypt.hashSync(req.body.pass, saltRounds);
+  }
 
   try {
     const usuarioDB = await User.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
